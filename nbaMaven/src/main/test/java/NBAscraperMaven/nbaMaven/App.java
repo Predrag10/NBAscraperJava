8b06256ff088cc4201c274b72b68e76702b1959e
Predrag Zvezdakoski <49346184+Predrag10@users.noreply.github.com>
2020-11-29 20:01:52 +0100

    Update App.java

--- nbaMaven/src/main/test/java/NBAscraperMaven/nbaMaven/App.java
+++ nbaMaven/src/main/test/java/NBAscraperMaven/nbaMaven/App.java
@@ -9,10 +9,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
 	public static void findStats(WebDriver driver, String playerName) {
